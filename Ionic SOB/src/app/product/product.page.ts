31973b266c53ff4b5db1b17c84b889beac031dc5
Ahmad Bagus <50206121+ahmadbagus99@users.noreply.github.com>
2020-02-18 17:40:39 +0700

    Update Product Page typescript

--- Ionic SOB/src/app/product/product.page.ts
+++ Ionic SOB/src/app/product/product.page.ts
@@ -98,9 +98,9 @@ export class ProductPage implements OnInit {
       this.grand = 0;
       
       for(let ii = 0; ii<this.AddArray.length; ii++){
-        if(this.items[i]['Nama'] == this.AddArray[ii]['Product'] && this.AddArray[ii]['Qty'] > 0){
-          this.total = this.total + this.AddArray[ii]['Qty'];
-          this.grand = this.grand + parseInt(this.AddArray[ii]['Price']);
+        if(this.items[i]['Nama'] == this.AddArray[ii]['Product'] ){
+          this.total += this.AddArray[ii]['Qty'];
+          this.grand = this.total * parseInt(this.AddArray[0]['Price']);
         } 
       }
       this.ArrayInput2 = { 
